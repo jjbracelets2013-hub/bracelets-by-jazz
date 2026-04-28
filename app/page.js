9d94272b73ef8b5245d4ff22faa1c9afb1823da6
jjbracelets2013-hub <jjbracelets2013@gmail.com>
2026-04-28 15:56:47 -0500

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Bracelets By Jazz 💎</h1>
+      <p>Your colorful jewelry store</p>
+
+      <button style={{
+        padding: "10px 20px",
+        background: "blue",
+        color: "white",
+        border: "none",
+        borderRadius: "5px"
+      }}>
+        Buy Now
+      </button>
+    </div>
+  );
+}
